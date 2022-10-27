9687596778a6a06c5286c7d06ec9deb699e3e57c
Nmgix <frostdraky@gmail.com>
2022-10-27 21:22:19 +0300

    Added bio cell

--- src/components/CellsSuffering/example-data/ExampleData.tsx
+++ src/components/CellsSuffering/example-data/ExampleData.tsx
@@ -68,9 +68,10 @@ export const dataExample: NewsletterDataTypes[] = [
     title: "Github Stats",
     type: "git",
     gitData: {
-      commitsImg: "",
+      commitsImg: "../example-data/github-example.png",
+      issuesPersentage: 0,
       commitsPerYear: 408,
-      commitPersentage: 96,
+      codeReviewPersentage: 0,
       pullRequestsPersentage: 4,
     },
     sizes: [
@@ -150,37 +151,30 @@ export const dataExample: NewsletterDataTypes[] = [
     sizes: [
       {
         width: 2,
-        height: 2,
-      },
-      {
-        width: 2,
         height: 3,
       },
     ],
     // пока что временно ReactNode, а не ReactNode[]
     description: (
-      <>
+      <div className='cell-type-bio-content'>
+        <h3>Немного о себе</h3>
         <p>
           С 2019 года начал изучать базу веб-разработки (HTML, CSS, JS) на SoloLearn и freeCodeCamp. Параллельно начал
           обучаться в КБТ по специальности “Информационные системы и программирование”.
         </p>
-        ,
         <p>
           В 2020 году открыл для себя Udemy, купил пару курсов (React от Непомнящего, NodeJS от Минина и пр.) и
           параллельно начал пилить pet-проекты.
         </p>
-        ,
         <p>
           В 2021 (примерно) начал изучать TS (Typescript) и постепенно замещать им обыкновенный JS т.к. больше нравится
           синтаксис C#.
         </p>
-        ,
         <p>
           С момента начала изучения TS вплоть до сегодняшнего дня полностью перестал использовать ванильный JS и пишу
           приложения (фронт - React и бек - NodeJS+Express) только с типизацией (пока несильной).
         </p>
-        ,
-      </>
+      </div>
     ),
   },
 ];
