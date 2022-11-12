7ba00a5eb34ddeb0d35c7cee903b0364b49dd1aa
Nmgix <frostdraky@gmail.com>
2022-11-12 10:18:40 +0300

    Finally fixed fonts to work both in Storybook and NextJS

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,12 +6,21 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    {
-      name: "@storybook/preset-scss",
-    },
+    // {
+    //   name: "@storybook/preset-scss",
+    // },
   ],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
+  },
+  webpack: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "resolve-url-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../"),
+    });
+
+    return config;
   },
 };
